071dee76b5ad52a9f0737dba8fc9db9099ad5438
Rob Doneux <robertdoneux@gmail.com>
2025-01-15 13:51:03 +0000

    initialize project with TypeScript setup, build script, and basic logging

---
+++ build.ts
@@ -0,0 +1,17 @@
+import { build, BuildOptions, BuildResult, Message } from 'esbuild';
+
+(async () => {
+    const buildOptions: BuildOptions = { entryPoints: ['src/index.ts'], outdir: "./dist", bundle: true, minify: true }
+    const { warnings, errors }: BuildResult = await build(buildOptions);
+
+    logResult(warnings, errors)
+
+})();
+
+function logResult(warnings: Message[], errors: Message[]) {
+    console.log("--------------")
+    console.log("Build complete")
+    if (warnings.length) console.log("Warnings: ", warnings)
+    if (errors.length) console.log("Errors: ", errors)
+    console.log("--------------")
+}
