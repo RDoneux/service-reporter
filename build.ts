59eccd5047a185a9b8cc9eb75a42bbd95b0ee766
Rob Doneux <robertdoneux@gmail.com>
2025-01-15 13:59:20 +0000

    add ESLint configuration and linting script; set up pre-commit hook

--- build.ts
+++ build.ts
@@ -1,17 +1,21 @@
-import { build, BuildOptions, BuildResult, Message } from 'esbuild';
+import { build, BuildOptions, BuildResult, Message } from "esbuild";
 
 (async () => {
-    const buildOptions: BuildOptions = { entryPoints: ['src/index.ts'], outdir: "./dist", bundle: true, minify: true }
-    const { warnings, errors }: BuildResult = await build(buildOptions);
+  const buildOptions: BuildOptions = {
+    entryPoints: ["src/index.ts"],
+    outdir: "./dist",
+    bundle: true,
+    minify: true,
+  };
+  const { warnings, errors }: BuildResult = await build(buildOptions);
 
-    logResult(warnings, errors)
-
+  logResult(warnings, errors);
 })();
 
 function logResult(warnings: Message[], errors: Message[]) {
-    console.log("--------------")
-    console.log("Build complete")
-    if (warnings.length) console.log("Warnings: ", warnings)
-    if (errors.length) console.log("Errors: ", errors)
-    console.log("--------------")
+  console.log("--------------");
+  console.log("Build complete");
+  if (warnings.length) console.log("Warnings: ", warnings);
+  if (errors.length) console.log("Errors: ", errors);
+  console.log("--------------");
 }
